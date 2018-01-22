d5d8aa0ba0684a06276ff0d4523f21ea48e1f2ba
Sonny Kurniawan <sonny.kurniawan.yap@gmail.com>
2018-01-22 12:29:39 +0900

    Added libraries for XtionSDK
    added libraries and a method to check xtion device ready

--- src/include/common.hpp
+++ src/include/common.hpp
@@ -1,5 +1,8 @@
 #define common_api __declspec(dllexport) 
 
+#include <OpenNI.h>
+
 extern "C" {
 	common_api char* TestHello();
+	common_api int InitDevice();
 }
--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,21 @@
 #include <iostream>
 
 using namespace std;
-
+using namespace openni;
 extern "C" {
 	char* TestHello() {
 		static char str[100];
 		sprintf(str, "Hello from native");
 		return str;
+	}
+
+	int InitDevice() {
+		Status rc = OpenNI::initialize();
+		if (rc != STATUS_OK)
+		{
+			printf("Initialize failed\n%s\n", OpenNI::getExtendedError());
+			return 1;
+		}
+		return 0;
 	}
 }
